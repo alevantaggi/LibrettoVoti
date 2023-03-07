e7bd7698a54206702aa35312ed68adb36815a178
Ale Vantaggi <Ale Vantaggi@172.22.132.33>
2023-03-07 13:23:46 +0100

    *codice fino al punto 5

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -1,5 +1,58 @@
 package it.polito.tdp.libretto.model;
 
-public class Libretto {
+import java.util.*;
 
+public class Libretto {
+	private List<Voto> voti;
+
+	public Libretto() {
+	this.voti= new ArrayList<>();
+	}
+	
+	
+/**
+ * Aggiungi un nuovo voto al libretto
+ * (per ora non fare nessun controllo)
+ * @param v Voto da aggiungere
+ * @return {@code true}
+ */
+	public boolean add(Voto v) {
+		 return voti.add(v);
+	}
+	
+	public void stampa() {
+		for (Voto v: voti) {
+			System.out.println(v);
+		}
+	}
+	
+	public void stampaPuntiUguali(int valore) {
+		for (Voto v: voti) {
+			if(v.getPunti()==valore) 
+				System.out.println(v);			
+		}
+	}
+	
+	public Voto cercaVoto(String nomeCorso) {
+		
+		for (Voto v: voti) {
+			if (v.getCorso().compareTo(nomeCorso)==0) {
+				return v;
+			}
+		}
+		
+		return null;
+		//throw new RuntimeException("Voto non trovato"); // Devo riservare le eccezioni per casi di errore 
+	}
+
+	
+	public boolean esisteVoto (Voto nuovo) {
+		
+		for (Voto v: voti) {
+			if (v.equalsCorsoPunti(nuovo)) 
+				return true;
+			}
+		return false;
+	}
+	
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -1,9 +1,26 @@
 package it.polito.tdp.libretto.model;
 
+import java.time.LocalDate;
+
 public class TestLibretto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Libretto libretto= new Libretto();
+		
+		libretto.add(new Voto("Analisi 1", 29, LocalDate.of(2021, 02, 15)));
+		libretto.add(new Voto("Fisica 2", 21, LocalDate.of(2022, 06, 10)));
+		libretto.add(new Voto("Informatica", 25, LocalDate.of(2021, 07, 01)));
+		
+		libretto.stampaPuntiUguali(25);
+		
+		System.out.println(libretto.cercaVoto("Analisi 1"));
+		System.out.println(libretto.cercaVoto("Analisi 2"));
+		
+		Voto a1bis= new Voto("Analisi 1", 29, LocalDate.of(2021, 02, 15));
+		Voto a1ter= new Voto("Analisi 1", 30, LocalDate.of(2021, 02, 15));
+		
+		System.out.println(a1bis+" è duplicato: "+ libretto.esisteVoto(a1bis));
+		System.out.println(a1ter+" è duplicato: "+ libretto.esisteVoto(a1ter));
 
 	}
 
